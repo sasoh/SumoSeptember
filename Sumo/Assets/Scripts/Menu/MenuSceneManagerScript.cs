86182070cc0950d907cdd7ee111c1d6075e69376
Alexander Popov <lxndrppv@gmail.com>
2015-10-04 02:12:57 +0300

    [+] added menu scene functions [c] hidden credits screen button for now

---
+++ Sumo/Assets/Scripts/Menu/MenuSceneManagerScript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuSceneManagerScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        // reset scores for players
+        PlayerPrefs.SetInt("Score1", 0);
+        PlayerPrefs.SetInt("Score2", 0);
+        PlayerPrefs.Save();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    /// <summary>
+    /// Loads match scene after pressing the play button.
+    /// </summary>
+    public void DidPressButtonPlay()
+    {
+        Application.LoadLevel("MatchScene");
+    }
+
+    /// <summary>
+    /// Loads credits scene.
+    /// </summary>
+    public void DidPressButtonCredits()
+    {
+
+    }
+
+    /// <summary>
+    /// Quits the game.
+    /// </summary>
+    public void DidPressButtonExit()
+    {
+        Application.Quit();
+    }
+}
